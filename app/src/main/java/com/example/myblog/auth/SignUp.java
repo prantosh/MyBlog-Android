59667f4eebf6e0e948be72c56c7f9fd3d0920add
prantosh <prantoshdeb@yahoo.com>
2019-06-18 08:50:08 +0530

    Sign up updated

---
+++ app/src/main/java/com/example/myblog/auth/SignUp.java
@@ -0,0 +1,17 @@
+package com.example.myblog.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myblog.R;
+
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
